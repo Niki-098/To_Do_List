dd26c3d528c1ef546a128c782cbb5536710bd59a
Nikita <kniki098@gmail.com>
2024-04-09 20:01:16 +0530

    Commit

--- home/views.py
+++ home/views.py
@@ -1,10 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from home.models import Task
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    context = {'success' : False}
+    if request.method == 'POST':
+        #Handle the form
+        title = request.POST['title']
+        desc = request.POST['desc']
+        print(title,desc)
+        ins = Task(taskTitle = title, taskDesc =desc)
+        ins.save()
+        context = {'success' : True}
+        
+        
+    return render(request, 'index.html', context)
 
 
 def tasks(request):
